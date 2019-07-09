e04dd5ed2604047b8d0b8bc89eccce7bc6683d6c
Defqon8 <bram.robyn@student.howest.be>
2019-07-09 10:27:32 +0200

    commit didn't work as expected

--- PersonTracking/client/main.js
+++ PersonTracking/client/main.js
@@ -1,48 +1,103 @@
 import { Template } from 'meteor/templating';
 import { _ } from 'meteor/underscore';
 import { ReactiveVar } from 'meteor/reactive-var';
-import "/import/genericList.html"
-
-
+import '../import/ui/CollectedJS.js'
 import './main.html';
+import '../import/ui/adminMenu.html'
+import { pictures } from '../import/api/pictures.js' ;
+import { devices } from '../import/api/devices.js' ;
+import { locations } from '../import/api/locations.js' ;
+
+
 
 Router.route('/', function () {
-  this.layout('Live-Data');
-  this.render('Menu', {to: 'Menu'});
-  this.render('LiveDataPage');
+  this.layout('Menu')
 });
 
-Router.route('/demodata', function () {
-  this.layout('Demo-Data');
-  this.render('Menu', {to: 'Menu'});
-  this.render('DemoDataPage');
+Router.route('/tracker', function () {
+  this.layout('');
+  this.render('');
 });
 
-Router.route('/collections', function () {
-  this.layout('Collections-Data');
-  this.render('Menu', {to: 'Menu'});
-  this.render('CollectionsPage');
-});
+Router.route('admin',function(){
+  this.layout('adminLayout');
+  this.render('adminMenu')
+})
 
-Router.route('/accounts', function () {
-  this.layout('Accounts-Data');
-  this.render('Menu', {to: 'Menu'});
-  this.render('AccountsPage');
-});
+// #region users
+Router.route('admin/users',function(){
+  this.layout('adminLayout');
+  this.render('genericUserList')
+})
+
+//form doesn't submit
+Router.route('admin/user/add',function(){
+  this.layout('adminLayout');
+  this.render('genericAddUser', {data:{pictures}})
+})
+
+Router.route('admin/user/:name',{
+  name: "edit.user",
+  action: function(){
+    var edituser = pictures.find({username: this.params.name}).fetch();
+    this.layout('adminLayout');
+    this.render('editUserHolder', {data:{pictures: edituser[0]}})
+  },
+  fastrender: true,
+  pictures: this
+})
+// #endregion
+
+// #region devices
+Router.route('admin/devices',function(){
+  this.layout('adminLayout');
+  this.render('genericDeviceList')
+})
 
 
-export const Students = new Mongo.Collection('Students');
- 
-Template.genericList.helpers({
-  generics() {
-    console.log(Students.find());
-    
-    return Students.find() ;
- }
-});
+Router.route('admin/devices/add',function(){
+  this.layout('adminLayout');
+  this.render('genericAddDevice', {data:{devices}})
+})
 
-Router.route('/collections/students', function(){
-  this.subscribe("Students");
-  this.layout('Collections-Data');
-  this.render('genericList',{});
-  });
+Router.route('admin/devices/:name',{
+  name: "edit.devices",
+  action: function(){
+    var editdevice = devices.find({MAC: this.params.name}).fetch();
+    this.layout('adminLayout');
+    this.render('genericEditDevice', {data:{devices: editdevice[0]}})
+  },
+  fastrender: true,
+  devices: this
+})
+// #endregion
+
+// #region locations
+Router.route('admin/locations',function(){
+  this.layout('adminLayout');
+  this.render('genericLocationList')
+})
+
+Router.route('admin/locations/add',function(){
+  this.layout('adminLayout');
+  this.render('genericAddLocation', {data:{locations}})
+})
+
+Router.route('admin/locations/:name',{
+  name: "edit.location",
+  action: function(){
+    var editlocation = devices.find({location: this.params.name}).fetch();
+    this.layout('adminLayout');
+    this.render('genericEditDevice', {data:{locations: editlocation[0]}})
+  },
+  fastrender: true,
+  locations: this
+})
+//#endregion
+
+
+
+
+
+
+
--- PersonTracking/server/main.js
+++ PersonTracking/server/main.js
@@ -1,4 +1,8 @@
 import { Meteor } from 'meteor/meteor';
+import '../import/api/pictures.js';
+import '../import/api/locations.js';
+import '../import/api/devices.js';
+
 
 Meteor.startup(() => {
   // code to run on server at startup
